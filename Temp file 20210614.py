8d15b16e95dc4ef64196cba62dbcfa68ac5ad0db
Uhkenneth <lauckk@gmail.com>
2021-07-04 12:43:08 +0800

    modigy

---
+++ Temp file 20210614.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import MyTime
+
+print(MyTime.GetTime("%Y %B %d"))
+print(MyTime.GetTime("%d %b %a"))
+print(MyTime.GetTime("%D %A"))
